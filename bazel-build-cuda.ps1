f99e8010477696f17c8f08895fa2e60fb5dc0a15
Cloud Han <cloudhan@outlook.com>
2021-07-30 22:08:58 +0800

    init

---
+++ bazel-build-cuda.ps1
@@ -0,0 +1,102 @@
+param(
+    # [Parameter(Mandatory = $true)]
+    # [String]$conda_env,
+
+    [Parameter(Mandatory = $true)]
+    [ValidateSet('11.2', '10.1')]
+    [String]$cuda_version,
+
+    [Parameter(Mandatory = $false)]
+    [String]$cuda_prefix = "C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA"
+)
+
+. (Join-Path (Split-Path $MyInvocation.MyCommand.Path) functions.ps1)
+$ErrorActionPreference = "Stop"
+
+# path for patch.exe and realpath.exe
+$msys2_root = 'C:\msys64'
+$msys2_path = "$msys2_root\usr\bin"
+
+switch ($cuda_version) {
+    '11.2' {
+        $cuda_cap = '6.1,7.0,7.5,8.0,8.6'
+        $cudnn_version = '8.1.1'
+    }
+    '10.1' {
+        $cuda_cap = '6.1,7.0,7.5'
+        $cudnn_version = '7.6.5'
+    }
+}
+
+$cuda_path = "$cuda_prefix/v$cuda_version"
+$cudnn_path = $cuda_path
+
+[System.Collections.ArrayList]$new_path = `
+    'C:\Windows\System32', `
+    'C:\Windows', `
+    'C:\Windows\System32\Wbem', `
+    'C:\Windows\System32\WindowsPowerShell\v1.0'
+
+Push-Environment
+Push-Location
+
+try {
+    # https://github.com/tensorflow/tensorflow/blob/9e2743271dd09609e8726edaffdd7c6762d3bf05/third_party/gpus/find_cuda_config.py#L26-L33
+    # and tf 2.0 release note
+    if ($cuda_path -eq $cudnn_path) {
+        # https://github.com/tensorflow/tensorflow/issues/51040
+        $env:TF_CUDA_PATHS="$cuda_path"
+    }
+    else {
+        $env:TF_CUDA_PATHS="$cuda_path,$cudnn_path"
+    }
+
+    # insert your path here
+    $new_path.Insert(0, 'C:\Program Files\Git\cmd')
+    $new_path.Insert(0, 'C:\tools\bazelisk')
+    $new_path.Insert(0, "$msys2_path")
+
+    if ($env:pythonLocation) {
+        # bring github actions python into path
+        $new_path.Insert(0, "$env:pythonLocation")
+        $new_path.Insert(0, "$env:pythonLocation/Scripts")
+    }
+
+    $env:PATH = $new_path -join ";"
+
+    Set-VSEnv
+
+    # bring conda python into environment, this supersede MSYS2's python and
+    # maybe VS's python
+    # conda activate $conda_env
+
+    # auto-detection is causing M$ banner/header/logo being included in the the
+    # vcpath, workaround it.
+    $env:BAZEL_VC = $env:VCINSTALLDIR
+
+    $cuda_tag = "cuda" + ($cuda_version -split "\." -join "")
+    $disk_cache = Join-Path (pwd) "bazel-build-cache" "$cuda_tag"
+
+    $env:PYTHONUNBUFFERED='1'
+
+    python .\build\build.py `
+        --enable_cuda `
+        --cuda_version="$cuda_version" `
+        --cuda_path="$cuda_path" `
+        --cudnn_version="$cudnn_version" `
+        --cudnn_path="$cudnn_path" `
+        --cuda_compute_capabilities="$cuda_cap"
+
+    if ((ls dist).Count -ne 1) {
+        throw "number of whl files != 1"
+    }
+    $name = (ls dist)[0].Name
+    $new_name = $name.Insert($name.IndexOf("-", $name.IndexOf("-") + 1), "+$cuda_tag")
+
+    mkdir "bazel-win-dist/$cuda_tag" -ErrorAction 0
+    mv -Force "dist/${name}" "bazel-win-dist/$cuda_tag/$new_name"
+}
+finally {
+    Pop-Location
+    Pop-Environment
+}
---
+++ functions.ps1
@@ -0,0 +1,75 @@
+# copy pasted from https://gist.github.com/cloudhan/97db3c1e57895a09a80ec1f30c471cb3
+function Set-EnvFromCmdSet {
+    [CmdletBinding()]
+    param(
+        [Parameter(ValueFromPipeline)]
+        [string]$CmdSetResult
+    )
+    process {
+        if ($CmdSetResult -Match "=") {
+            $i = $CmdSetResult.IndexOf("=")
+            $k = $CmdSetResult.Substring(0, $i)
+            $v = $CmdSetResult.Substring($i + 1)
+            Set-Item -Force -Path "Env:\$k" -Value "$v"
+        }
+    }
+}
+
+function Set-VSEnv {
+    param (
+        [parameter(Mandatory = $false)]
+        [ValidateSet(2017, 2019)]
+        [int]$Version = 2019,
+
+        [parameter(Mandatory = $false)]
+        [ValidateSet("all", "x86", "x64")]
+        [String]$Arch = "x64"
+    )
+
+    $vs_where = 'C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe'
+
+    $vs_env = @{ }
+
+    if ($Version -eq 2019) {
+        $vs_env = @{
+            install_path = (&$vs_where -version '[16,17)'  -format json | ConvertFrom-Json)[0].installationPath
+            all          = 'Common7\Tools\VsDevCmd.bat'
+            x64          = 'VC\Auxiliary\Build\vcvars64.bat'
+            x86          = 'VC\Auxiliary\Build\vcvars32.bat'
+        }
+    }
+    else {
+        $vs_env = @{
+            install_path = (&$vs_where -version '[15,16)'  -format json | ConvertFrom-Json)[0].installationPath
+            all          = 'Common7\Tools\VsDevCmd.bat'
+            x64          = 'VC\Auxiliary\Build\vcvars64.bat'
+            x86          = 'VC\Auxiliary\Build\vcvars32.bat'
+        }
+    }
+
+    if ( $null -eq $vs_env.install_path) {
+        Write-Host -ForegroundColor Red "Visual Studio $Version is not installed."
+        return
+    }
+
+    $path = Join-Path $vs_env.install_path $vs_env.$Arch
+
+    C:/Windows/System32/cmd.exe /c "`"$path`" & set" | Set-EnvFromCmdSet
+    Write-Host -ForegroundColor Green "Visual Studio $Version $Arch Command Prompt variables set."
+}
+
+
+class EnvironmentStack {
+    static [System.Collections.Stack] $stack = [System.Collections.Stack]::new()
+}
+
+function Push-Environment {
+    [EnvironmentStack]::stack.push([Environment]::GetEnvironmentVariables())
+}
+
+function Pop-Environment {
+    [EnvironmentStack]::stack.pop().GetEnumerator() |
+    ForEach-Object {
+        Set-Item -Force -Path ("Env:\" + $_.Key) -Value $_.Value
+    }
+}
